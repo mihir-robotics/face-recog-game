21f69b6916e3c98f6c78d81c58af2e16790aad5c
mihir-robotics <mihir.kulkarni.808@gmail.com>
2023-11-28 16:34:20 +0530

    changes -run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 # Execute the faceGame2.py script with arguments
+# Temp. bash file for ease of execution
 #!/bin/bash
 python faceGame2.py --prototxt deploy.prototxt.txt --model res10_300x300_ssd_iter_140000.caffemodel
