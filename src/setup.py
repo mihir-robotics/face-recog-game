e0e92cf6ee1db150ba2a0d356b1a8e4eef60b03b
mihir-robotics <mihir.kulkarni.808@gmail.com>
2023-11-30 11:13:17 +0530

    added documentation -setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,40 @@
-# File creates the FILEPATHS for PNGS ASSETS, MODEL FILES ETC
+# Simple code to find absolute paths of file w.r.t working dir.
+# Can be expanded to obtain game configuration as well
 
+# Import os for req. functions
 import os
 
 def find_absolute_paths():
+    '''
+    Find absolute paths for specific files located within sub-folders.
+
+    This function retrieves the absolute paths for three files: 
+    'deploy.prototxt.txt',
+    'res10_300x300_ssd_iter_140000.caffemodel', 
+    'player.png'. 
+    Files are expected to be located within respective sub-folders ('models' and 'assets/images')
+    relative to the current working directory.
+
+    Returns:
+        tuple: A tuple containing the absolute paths for the three specified files.
+               (MODEL_absolute, WEIGHTS_absolute, PNG_absolute)
+
+    Note:
+        Ensure the files exist within their corresponding sub-folders to retrieve
+        their absolute paths successfully; or make the required changes in the MODEL, WEIGHTS, PNG
+        variables
+    '''
+    # Define the files w/ relative path:
+    model = 'model\\deploy.prototxt.txt'
+    weights = 'model\\res10_300x300_ssd_iter_140000.caffemodel'
+    png = 'assets\\player.png'
+
+    # Try to find paths; throw error if paths cannot be resolved
     try:
         # Find absolute paths for the three files
-        MODEL_path = os.path.abspath('model\\deploy.prototxt.txt')
-        WEIGHTS_path = os.path.abspath('model\\res10_300x300_ssd_iter_140000.caffemodel')
-        PNG_path = os.path.abspath('assets\\player.png')
+        MODEL_path = os.path.abspath(model)
+        WEIGHTS_path = os.path.abspath(weights)
+        PNG_path = os.path.abspath(png)
 
         # Store the absolute paths as raw string variables
         MODEL_absolute = rf'{MODEL_path}'
